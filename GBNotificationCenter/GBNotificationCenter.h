d6ee88f053f2140aaaaf95f1277763f369c9a655
Luka Mirosevic <luka@goonbee.com>
2013-04-01 14:54:43 +0200

    Initial Commit

---
+++ GBNotificationCenter/GBNotificationCenter.h
@@ -0,0 +1,13 @@
+//
+//  GBNotificationCenter.h
+//  GBNotificationCenter
+//
+//  Created by Luka Mirosevic on 01/04/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GBNotificationCenter : NSObject
+
+@end
---
+++ GBNotificationCenter/GBNotificationCenter.m
@@ -0,0 +1,13 @@
+//
+//  GBNotificationCenter.m
+//  GBNotificationCenter
+//
+//  Created by Luka Mirosevic on 01/04/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import "GBNotificationCenter.h"
+
+@implementation GBNotificationCenter
+
+@end
